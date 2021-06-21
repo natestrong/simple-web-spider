1a21528e953508c446dec59d9be98c02790ddd43
nstrong <nstrong@spanimation.com>
2021-06-21 16:57:15 -0700

    TaskQueue to handle concurrency

---
+++ v2/taskQueue.ts
@@ -0,0 +1,31 @@
+import EventEmitter from "events";
+
+export class TaskQueue extends EventEmitter {
+    concurrency: number;
+    running: number;
+    queue: any[];
+
+    constructor(concurrency) {
+        super();
+        this.concurrency = concurrency;
+        this.running = 0;
+        this.queue = [];
+    }
+
+    pushTask(task) {
+        this.queue.push(task);
+        process.nextTick(this.next.bind(this));
+        return this;
+    }
+
+    next() {
+        while (this.running < this.concurrency && this.queue.length) {
+            const task = this.queue.shift();
+            task(() => {
+                this.running--;
+                process.nextTick(this.next.bind(this));
+            });
+            this.running++;
+        }
+    }
+}
